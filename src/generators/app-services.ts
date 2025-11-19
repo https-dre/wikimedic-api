ef9d151f9e20c1a5588c0f780ef76845ef1fe922
https-dre <diaso.andre@outlook.com>
2025-11-18 21:56:25 -0300

    fix(generator): remove unused repository

--- src/generators/app-services.ts
+++ src/generators/app-services.ts
@@ -1,22 +1,21 @@
 import { db } from "../data/postgresql/db";
 import { JwtProvider } from "../providers/crypto-provider";
-import { MedicamentoRepository } from "../repositories/mongo/MedicamentoRepository";
 import { AppointmentRepository, UserSqlRepository } from "../repositories";
 import { MedicService } from "../services/medic-service";
 import { UserService } from "../services/user-service";
 import { AppointmentService } from "../services/appointment-service";
+import { MedicineRepository } from "@/repositories/postgresql/MedicineRepository";
 
 const jwtProvider = new JwtProvider();
 const userSqlRepository = new UserSqlRepository(db);
 const appointmentRepository = new AppointmentRepository(db);
-
-const medRepository = new MedicamentoRepository();
+const medicineRepository = new MedicineRepository(db);
 
 const appServices = {
   user: new UserService(userSqlRepository, jwtProvider),
-  med: new MedicService(medRepository),
+  med: new MedicService(medicineRepository),
   appointment: new AppointmentService(
-    medRepository,
+    medicineRepository,
     appointmentRepository,
     userSqlRepository
   ),
