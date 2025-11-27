4367c166904891e9ebea78628a618d02cf3ce64f
https-dre <diaso.andre@outlook.com>
2025-11-27 19:59:18 -0300

    feat: add route to authenticate user

--- src/routers/user-routes.ts
+++ src/routers/user-routes.ts
@@ -3,9 +3,11 @@ import { createUser } from "./users/create-user";
 import { appServices } from "../generators/app-services";
 import { deleteUser } from "./users/delete-user";
 import { updateUser } from "./users/update-user";
+import { userLogin } from "./users/user-login";
 
 export const user_routes = (app: FastifyInstance) => {
   app.register(createUser(appServices.user));
   app.register(deleteUser(appServices.user));
   app.register(updateUser(appServices.user));
+  app.register(userLogin(appServices.user));
 };
---
+++ src/routers/users/user-login.ts
@@ -0,0 +1,34 @@
+import { zUser } from "@/models/User";
+import { UserService } from "@/services/user-service";
+import { FastifyPluginAsync } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+
+export const userLogin = (service: UserService): FastifyPluginAsync => {
+  return async (app) => {
+    app.withTypeProvider<ZodTypeProvider>().patch(
+      "/users/auth",
+      {
+        schema: {
+          summary: "Authenticate user",
+          tags: ["users"],
+          body: z.object({
+            email: z.string().email(),
+            password: z.string(),
+          }),
+          response: {
+            201: z.object({
+              token: z.string().jwt(),
+              user: zUser.omit({ password: true }),
+            }),
+          },
+        },
+      },
+      async (req, reply) => {
+        const { email, password } = req.body;
+        const payload = await service.genAuth(email, password);
+        return reply.status(201).send(payload);
+      }
+    );
+  };
+};
