2f941d2d2b12996928c2b97824ca1fdc251bd623
https-dre <diaso.andre@outlook.com>
2025-12-02 14:58:38 -0300

    chore: add type file to appointments

---
+++ src/types/appointment.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const zTreatment = z.object({
+  id: z.string().uuid(),
+  start_time: z.coerce.date(),
+  end_time: z.coerce.date(),
+  repetition: z.coerce.number(),
+  repetition_unit: z.string(),
+  amount: z.coerce.number(),
+  dosage_unit: z.string(),
+  color: z.string(),
+  commercial_name: z.string(),
+  taken_count: z.coerce.number(),
+});
+
+export const zTreatmentSummary_Item = z.object({
+  id: z.string().uuid(),
+  medicine_name: z.string(),
+  dosage: z.string(),
+  color: z.string(),
+  stats: z.object({
+    taken: z.coerce.number(),
+    total: z.coerce.number(),
+    percentage: z.coerce.number(),
+  }),
+});
+
+export type Treatment = z.infer<typeof zTreatment>;
+export type TreatmentSummary_Item = z.infer<typeof zTreatmentSummary_Item>;
